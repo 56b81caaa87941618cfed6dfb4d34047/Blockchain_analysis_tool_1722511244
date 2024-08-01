69cecce078f159166e3d33f03ff4fc64da94bdb4
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-01 16:51:02 +0530

    Placeholder text replacement

--- components/simple_footer_component_1722511248.js
+++ components/simple_footer_component_1722511248.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1722511248", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            BlockAnalyzer - Decentralized Blockchain Analytics
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Stay ahead of the curve with real-time insights into the latest blockchain data. BlockAnalyzer provides advanced analytics and monitoring tools to help you make informed decisions in the dynamic world of decentralized networks.
                     </div>
                 </div>
             </div>
